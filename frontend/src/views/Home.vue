a0e97ed5142ee29c0effd06c03702ad079aaef31
Hoshinopb <2014980462@qq.com>
2025-12-27 14:13:53 +0800

    完成基础框架

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <el-container style="height: 100vh">
+    <el-header>图片管理网站</el-header>
+    <el-main>
+      <el-empty description="前端环境已配置完成" />
+    </el-main>
+  </el-container>
+</template>
+
+<script setup>
+</script>
